e9bca67adf74f75bd180c814d55ba9e3a20d6a2e
nathanshaw <nathanshawsemail@gmail.com>
2016-12-06 01:45:59 -0800

    added Homados

---
+++ Homados/firmware/homados_firmware/homados_firmware.ino
@@ -0,0 +1,122 @@
+/*
+  Standard firmware for the Homados 16 channel solinoid driver
+ The first 9 channels are PWM driven while the remaining 7 channels are digital
+ 
+ When loading the firmware onto an Arduino Mega take note of
+ the following steps:
+ 
+ 1. if you want the bot to silently boot (not trigger its solinoids) 
+ set SILENT_BOOT to 1
+ 2. set LED_FEEDBACK to 1 if you want the LED's to provide runtime feedback
+ 3. Assign a new ARDUINO_ID: the Homados boards are 11 - 20
+ 
+ */
+
+# define NUM_SOLENOIDS 16
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#define LED_POWER 11
+#define LED_STATUS 12
+#define ARDUINO_ID 11
+#define LED_FEEDBACK 1
+#define SILENT_BOOT 0
+
+char bytes[2];
+short notes[NUM_SOLENOIDS];
+
+int handshake = 0;
+int statustimer = 0;
+
+// actuator pins
+int actuators[] = {
+  2, 3, 4, 5, 6, 7, 8, 9, 10, 
+  22, 24, 26, 28, 30, 32
+};
+
+void setup() {
+  Serial.begin(57600);
+
+  // interrupt timer parameters, these need to change
+  //TCCR2A = 1;
+  //TCCR2B = 3;
+  //TIMSK2 = 1;
+
+  pinMode(LED_POWER, OUTPUT);
+  pinMode(LED_STATUS, OUTPUT);
+  digitalWrite(LED_STATUS, HIGH);
+  digitalWrite(LED_POWER, HIGH);
+
+  for (int i = 0; i < NUM_SOLENOIDS; i++) {
+    pinMode(actuators[i], OUTPUT);
+    if (SILENT_BOOT == 0){
+      digitalWrite(actuators[i], HIGH);
+      delay(30);
+      digitalWrite(actuators[i], LOW);
+      delay(100);  
+    }
+    digitalWrite(actuators[i], LOW);
+  }
+  delay(1000);
+  digitalWrite(LED_POWER, LOW);
+}
+
+ISR(TIMER2_OVF_vect) {
+  // solenoid control
+  for (int i = 0; i < NUM_SOLENOIDS; i++) {
+    if (notes[i] > 0) {
+      digitalWrite(actuators[i], HIGH);
+      notes[i]--;
+    }
+    else {
+      digitalWrite(actuators[i], LOW);
+    }
+  }
+  if (statustimer > 0 && LED_FEEDBACK) {
+    digitalWrite(LED_STATUS, HIGH);
+    statustimer--;
+  }
+  else {
+    digitalWrite(LED_STATUS, LOW);
+  }
+}
+// 15 is broken
+//13 is broken?
+void loop() {
+  for (int i = 0; i < NUM_SOLENOIDS; i++) {
+    digitalWrite(actuators[i], HIGH);
+    digitalWrite(LED_STATUS, LOW);
+    delay(50); 
+    digitalWrite(LED_STATUS, HIGH);
+    digitalWrite(actuators[i], LOW);
+    delay(100); 
+  }
+  /*
+  if (Serial.available()) {
+    if (Serial.read() == 0xff) {
+      // reads in a two index array from ChucK
+      Serial.readBytes(bytes, 2);
+
+      // bit wise operations
+      // ~~~~~~~~~~~~~~~~~~~
+      // reads the first six bits for the note number
+      // then reads the last ten bits for the note velocity
+      int pitch = byte(bytes[0]) >> 2;
+      int velocity = (byte(bytes[0]) << 8 | byte(bytes[1])) & 1023;
+
+      // message required for "handshake" to occur
+      // happens once per Arduino at the start of the ChucK serial code
+      if (pitch == 63 && velocity == 1023 && handshake == 0) {
+        Serial.write(ARDUINO_ID);
+        handshake = 1;
+      }   
+      if (pitch >= 0 && pitch <= NUM_SOLENOIDS) {
+        statustimer = 120;
+        notes[pitch] = (velocity * 0.5);
+      }
+    }
+  }
+  */
+}
+
